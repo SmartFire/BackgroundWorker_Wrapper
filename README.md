0962e352ecbd3c82a837afd0f647d8cea00ae5f0
Justin Packwood <justin@zodiacempires.com>
2015-02-09 20:13:58 -0500

    Readme Update
    Just updating the readme.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # BackgroundWorker_Wrapper
 This is a wrapper class I have written for the System.ComponentModel.BackgroundWorker class. 
 It provides many methods to assist you when it comes to working with a large number of BackgroundWorkers.
+
+Documentation and Examples can be found [here](http://zejustin.info/BackgroudWorker_Wrapper/)
---
+++ source/BGHelper/Examples.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BgHelper
+{
+    class Examples
+    {
+        // Create a new instance of BgHelper with 200 workers, and tell it to not call DoEvents().
+        readonly BgHelper _workersHelper = new BgHelper(200, false); 
+
+        public Examples()
+        {
+            for (var i = 0; i < 200; i++)
+            {
+                // Grab the next available worker in the pool
+                BackgroundWorker worker = _workersHelper.GetNextWorker();
+                // Assign the DoWork event, or any other event.
+                worker.DoWork += WorkerOnDoWork;
+                // Start the worker, optionally passing an argument. 
+                worker.RunWorkerAsync();
+            }
+
+            // If you want to do something wile waiting for the workers to complete
+            while (!_workersHelper.CheckCompletion())
+            {
+                // Do Some Stuff
+
+                // If at any time you need to cancel all of your workers, simply call the method.
+                _workersHelper.CancelAllWorkers();
+                // Or, because we are using the thread collection in this example 
+                // we can use KillAllWorkers to forcefully abort the threads themselves.
+                _workersHelper.KillAllWorkers();
+            }
+
+            // Or if you simply want to wait until all workers complete instead, just use this.
+            _workersHelper.WaitForWorkers();
+        }
+
+        private void WorkerOnDoWork(object sender, DoWorkEventArgs doWorkEventArgs)
+        {
+            // Add this worker to the thread collection so that I can call KillAllWorkers later. 
+            // This is completely optional. Only use this if you plan to use KillAllWorkers
+            var index = _workersHelper.AddWorkerThread(Thread.CurrentThread);
+
+            // Perform some task here...
+            //...
+            //...
+
+            // Because I am using the thread collection, I need to reset this 
+            // workers location in the pool so the next worker can take its place.
+            _workersHelper.ResetWorkerThread(index);
+        }
+    }
+}
